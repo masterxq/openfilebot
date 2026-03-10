81fc565b840cc06c55a0d7bae95f7f1eac76659b
fk <fk@mutemail.de>
2026-03-10 05:28:52 +0100

    cleanup(native): remove obsolete armv7l libs

--- lib/native/update-mediainfo.sh
+++ lib/native/update-mediainfo.sh
@@ -10,9 +10,7 @@ mkdir -p "Staging" && cd "Staging"
 curl -O "https://mediaarea.net/download/binary/libmediainfo0/${MEDIAINFO_VERSION}/MediaInfo_DLL_${MEDIAINFO_VERSION}_Mac_x86_64+arm64.tar.bz2"
 curl -O "https://mediaarea.net/download/binary/libmediainfo0/${MEDIAINFO_VERSION}/MediaInfo_DLL_${MEDIAINFO_VERSION}_Windows_x64_WithoutInstaller.7z"
 curl -O "https://mediaarea.net/download/binary/libmediainfo0/${MEDIAINFO_VERSION}/libmediainfo0v5_${MEDIAINFO_VERSION}-1_amd64.Debian_12.deb"
-curl -O "https://mediaarea.net/download/binary/libmediainfo0/${MEDIAINFO_VERSION}/libmediainfo0v5_${MEDIAINFO_VERSION}-1_armhf.Raspbian_12.deb"
 curl -O "https://mediaarea.net/download/binary/libzen0/${ZEN_VERSION}/libzen0v5_${ZEN_VERSION}-1_amd64.Debian_12.deb"
-curl -O "https://mediaarea.net/download/binary/libzen0/${ZEN_VERSION}/libzen0v5_${ZEN_VERSION}-1_armhf.Raspbian_12.deb"
 
 # Linux i386 is intentionally not refreshed here as current upstream MediaInfo packages do not provide a matching i386 binary.
 
@@ -31,8 +29,6 @@ cd ..
 cp Staging/*Mac*x86_64+arm64*/*/*/libmediainfo.dylib mac-x86_64/libmediainfo.dylib
 cp Staging/*Windows*x64*/MediaInfo.dll win32-x64/MediaInfo.dll
 cp Staging/*/data/usr/lib/x86_64-linux-gnu/libmediainfo.so.0.0.0 linux-amd64/libmediainfo.so
-cp Staging/*/data/usr/lib/arm-linux-gnueabihf/libmediainfo.so.0.0.0 linux-armv7l/libmediainfo.so
 cp Staging/*/data/usr/lib/x86_64-linux-gnu/libzen.so.${ZEN_VERSION} linux-amd64/libzen.so
-cp Staging/*/data/usr/lib/arm-linux-gnueabihf/libzen.so.${ZEN_VERSION} linux-armv7l/libzen.so
 
 rm -r Staging
